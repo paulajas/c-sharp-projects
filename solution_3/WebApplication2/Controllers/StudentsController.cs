79e270cf5d54bfeb9949c0a6deb344fc6a34c4be
paulajas <paulajas@gmail.com>
2022-10-08 14:48:03 +0200

    Add new solution for next exercise

---
+++ solution_3/WebApplication2/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _service;
+        public StudentsController(IDbService cdService)
+        {
+            _service = cdService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(_service.GetStudents());
+        }
+
+        [HttpGet("{studentId}")]
+        public IActionResult GetStudent(int studentId)
+        {
+            return Ok(_service.GetStudent(studentId));
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody]Student newStudent)
+        {
+            if (_service.AddStudent(newStudent))
+            {
+                return Ok("Student added");
+            }
+            return BadRequest("Student is already in database");
+        }
+
+        [HttpPut("{studentId}")]
+        public IActionResult PutStudent([FromBody]Student student)
+        {
+            if (_service.UpdateStudent(student) != null)
+            {
+                return Ok(_service.UpdateStudent(student));
+            }
+            return BadRequest("Student is already in database");
+        }
+
+        [HttpDelete("{studentId}")]
+        public IActionResult DeleteStudent(int studentId)
+        {
+            if (_service.DeleteStudent(studentId))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Cannot drop student from database");
+            }
+        } 
+    }
+}
